95736262e44bfcf23b56f13126c5a5c7682fff06
IhnatVelykoivan <ihnat.velykoivan@gmail.com>
2025-09-22 17:01:57 +0300

    fix: update Vue script to production build and configure Vite build options

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [vue()],
+    base: '/Front-End-Developer-Trial-Task-Immersive-Cinematic-UI/',
     server: {
         port: 3000,
         historyApiFallback: true
+    },
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: false
     }
 })
